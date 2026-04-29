8b5890de1b68a70d4aaed9b6681a3ba3ce607992
Nani10011001 <nani1531323@gmail.com>
2026-04-29 16:33:50 +0530

    ResumeAgent

--- RA_Agent/NLP/spacy_ext.py
+++ RA_Agent/NLP/spacy_ext.py
@@ -1,6 +1,6 @@
 import spacy
 import re
-nlp =spacy.load("en_core_web_md")
+nlp =spacy.load("en_core_web_sm")
 Year_regex=re.compile(r"\b(19\d{2}|20\d{2})\b")
 Skill_Blacklist={
    "Python", "JavaScript", "SQL", "MongoDB",
