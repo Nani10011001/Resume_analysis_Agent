9e9014f4f60c67fa8328096b56053d8689c7d2ec
Nani10011001 <nani1531323@gmail.com>
2026-04-29 17:28:03 +0530

    resumeAgent

--- RA_Agent/main.py
+++ RA_Agent/main.py
@@ -5,22 +5,26 @@ from Config.logger import setup_logger
 from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
+import logging
+import uvicorn
+
 load_dotenv()
 setup_logger()
-import logging
+
 logger = logging.getLogger(__name__)
-import uvicorn
-app=FastAPI()
-app.include_router(chatAgentRouter)
-app.include_router(fileAgentRouter)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info("App started successfully")
     yield
     logger.info("App shutting down")
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(chatAgentRouter)
+app.include_router(fileAgentRouter)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
-   
-    
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
